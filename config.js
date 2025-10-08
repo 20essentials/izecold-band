f7642ebec1764dde2d96060874b2aaf93bba765a
DominicDev <s0l0estudi4r@gmail.com>
2025-10-08 18:24:31 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'IZECOLD';
+export let nameSongs = [
+	"CLOSE ", /*1*/
+	"SWIGGITY", /*2*/
+	"LULLABY", /*3*/
+	"MAN IN THE MIRROR", /*4*/
+	"HEARTS COLLIDE", /*5*/
+	"ENTROPY", /*6*/
+	"CHANGE", /*7*/
+	"OXIDIZE", /*8*/
+	"TOMORROW", /*9*/
+	"CATCH 'EM ALL", /*10*/
+	"THIS TIME", /*11*/
+	"CLOSE (REMIX)", /*12*/
+	"CLOSE - BROKE REMIX", /*13*/
+	"SYSTEM OVERLOAD", /*14*/
+	"BREAK THE SOUND", /*15*/
+	"UNDERGROUND", /*16*/
+	" REPRISAL", /*17*/
+	"DOOZY", /*18*/
+	"EVANESCENT", /*19*/
+	"ONCE I WAKE", /*20*/
+]
